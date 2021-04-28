ece3e7857ad876373d3b6348c065fa347b4562e9
Yura-Sokolovskyi <yuraprmm@gmail.com>
2021-04-28 16:23:27 +0300

    Added lemmas api

---
+++ src/Service/OxfordDictionary/Apis/LemmasApi.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Service\OxfordDictionary\Apis;
+
+
+
+use App\Service\OxfordDictionary\Builders\LemmasGetResultsBuilder;
+use App\Service\OxfordDictionary\Client\HttpClientInterface;
+
+class LemmasApi
+{
+    /**
+     * @var HttpClientInterface
+     */
+    private  HttpClientInterface $client;
+
+
+    /**
+     * LemmasApi constructor.
+     * @param HttpClientInterface $client
+     */
+    public function __construct(HttpClientInterface  $client)
+    {
+        $this->client = $client;
+    }
+
+
+    /**
+     * @param $source_lang
+     * @param $word_id
+     * @return array
+     */
+    public function get($source_lang, $word_id): array
+    {
+        $uri = sprintf(
+            '/lemmas/%s/%s',
+            $source_lang,
+            $word_id
+        );
+
+        $response = $this->client->get($uri);
+
+        $resultBuilder  = new LemmasGetResultsBuilder($response);
+
+        return $resultBuilder->build();
+    }
+}
---
+++ src/Service/OxfordDictionary/Builders/LemmasGetResultsBuilder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service\OxfordDictionary\Builders;
+
+
+use App\Service\OxfordDictionary\Models\LexicalEntries\LexicalEntry;
+
+class LemmasGetResultsBuilder
+{
+
+
+    /**
+     * @var array
+     */
+    private array $response;
+
+
+    /**
+     * LemmasGetResultsBuilder constructor.
+     * @param array $response
+     */
+    public function __construct(array $response)
+    {
+        $this->response = $response;
+    }
+
+
+    /**
+     * @return array
+     */
+    public function build(): array
+    {
+
+        $results = [];
+
+        if (key_exists('error', $this->response)) {
+            return $results;
+        }
+
+        foreach ($this->response['results'][0]['lexicalEntries'] as $entry) {
+
+            $results[] = new LexicalEntry([
+
+                'id'=>$this->response['results'][0]['id'],
+                'language'=>$entry['language'],
+                'inflectionOf' => $entry['inflectionOf'][0]['text'],
+                'lexicalCategory' => $entry['lexicalCategory']['text'],
+
+            ]);
+
+        }
+
+        return $results;
+    }
+}
