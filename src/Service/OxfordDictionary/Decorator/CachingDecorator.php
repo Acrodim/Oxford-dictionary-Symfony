756a640585e7cb51d14b71d44e73c561bc9e0192
Taras <tkoval37@gmail.com>
2021-05-02 14:15:14 +0300

    Feature CachingDecorator

---
+++ src/Service/OxfordDictionary/Decorator/CachingDecorator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Service\OxfordDictionary\Decorator;
+
+use App\Entity\Cache;
+use App\Service\OxfordDictionary\Client\HttpClientInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CachingDecorator implements HttpClientInterface
+{
+    private HttpClientInterface $client;
+    private EntityManagerInterface $em;
+
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $em)
+    {
+        $this->client = $client;
+        $this->em = $em;
+
+        dd("test");
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function get(string $uri): ?array
+    {
+        $uriHash = sha1($uri);
+
+        $cache = $this->em->getRepository(Cache::class)
+            ->findOneBy(['uriKey' => $uriHash]);
+
+        if ($cache) {
+            return $cache->getValue();
+        }
+
+        $response = $this->client->get($uri);
+
+        $cache = (new Cache())->setUriKey($uriHash)->setValue($response);
+        $this->em->persist($cache);
+        $this->em->flush();
+
+        return $cache->getValue();
+    }
+}
